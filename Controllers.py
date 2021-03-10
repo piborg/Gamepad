004ddc12d41c4d50c73605e826992cac31db5327
Spoonieau <spoonie.woonie@gmail.com>
2021-03-10 21:16:15 +0800

    Updated Steam controller mapping

--- Controllers.py
+++ Controllers.py
@@ -186,6 +186,74 @@ class Xbox360(Gamepad):
         }
         self._setupReverseMaps()
 
+class XboxONE(Gamepad):
+    fullName = 'Xbox ONE controller'
+
+    def __init__(self, joystickNumber = 0):
+        Gamepad.__init__(self, joystickNumber)
+        self.axisNames = {
+            0: 'LAS -X', #Left Analog Stick Left/Right
+            1: 'LAS -Y', #Left Analog Stick Up/Down
+            2: 'RAS -X', #Right Analog Stick Left/Right
+            3: 'RAS -Y', #Right Analog Stick Up/Down
+            4: 'RT', #Right Trigger
+            5: 'LT', #Left Trigger
+            6: 'DPAD -X', #D-Pad Left/Right
+            7: 'DPAD -Y' #D-Pad Up/Down
+        }
+        self.buttonNames = {
+            0:  'A', #A Button
+            1:  'B', #B Button
+            3:  'X', #X Button
+            4:  'Y', #Y Button
+            6:  'LB', #Left Bumper
+            7:  'RB', #Right Bumper
+            11: 'START', #Hamburger Button
+            12: 'HOME', #XBOX Button
+            13: 'LASB', #Left Analog Stick button
+            14: 'RASB' #Right Analog Stick button
+                
+        }
+        self._setupReverseMaps()
+        
+class Steam(Gamepad):
+    fullName = 'Steam controller'
+
+    def __init__(self, joystickNumber = 0):
+        Gamepad.__init__(self, joystickNumber)
+        self.axisNames = {
+            0: 'AS -X', #Analog Stick Left/Right
+            1: 'AS -Y', #Analog Stick Up/Down
+            2: 'RTP -X', #Right Track Pad Left/Right
+            3: 'RTP -Y', #Right Track Pad Up/Down
+            4: 'LTP -Y', #Left Track Pad Up/Down
+            5: 'LTP -X', #Left Track Pad Left/Right
+            6: 'RTA', #Right Trigger Axis
+            7: 'LTA' #Left Trigger Axis
+        }
+        self.buttonNames = {
+            0:  'LPTBUTTON', #Left TrackPad button
+            1:  'RTPBUTTON', #Right TrackPad button
+            2:  'A', #A Button
+            3:  'B', #B Button
+            4:  'X', #X Button
+            5:  'Y', #Y Button
+            6:  'LB', #Left Bumper
+            7:  'RB', #Right Bumper
+            8:  'LT', #Left Trigger
+            9:  'RT', #Right Trigger
+            10: 'SELECT', #Select Button <
+            11: 'START', #Start button >
+            12: 'HOME', #Steam Button
+            13: 'STICKBUTTON', #Analog Stick button
+            15: 'LG', #Left Grip
+            16: 'RG', #Right Grip
+            17: 'LTP -DUP', #Left TrackPad D-PAD Up
+            18: 'LTP -DDOWN', #Left TrackPad D-PAD Down
+            19: 'LTP -DLEFT', #Left TrackPad D-PAD Left
+            20: 'LTP -DRIGHT', #Left TrackPad D-PAD Right
+        }
+        self._setupReverseMaps()
 
 class MMP1251(Gamepad):
     fullName = "ModMyPi Raspberry Pi Wireless USB Gamepad"
@@ -239,40 +307,5 @@ class example(Gamepad):
             0: 'BUTTON0',
             1: 'BUTTON1',
             2: 'BUTTON2'
-        }
-        self._setupReverseMaps()
-
-class STEAM(Gamepad):
-    fullName = 'Steam controller'
-
-    def __init__(self, joystickNumber = 0):
-        Gamepad.__init__(self, joystickNumber)
-        self.axisNames = {
-            0: 'JOY-Y',
-            1: 'JOY-X',
-            2: 'TPR-Y',
-            3: 'TPR-X',
-            4: 'TPL-Y',
-            5: 'TPL-X',
-            6: 'DPAD-X',
-            7: 'DPAD-Y'
-        }
-        self.buttonNames = {
-            0:  'TPR CLICK',
-            1:  'TPL CLICK',
-            2:  'A',
-            3:  'B',
-            4:  'X',
-            5:  'Y',
-            6:  'L2',
-            7:  'R2',
-            8:  'L1',
-            9:  'R1',
-            10: 'SELECT',
-            11: 'START',
-            12: 'STEAM',
-            13: 'JOY CLICK',
-            15: 'LG',
-            16: 'RG'
         }
         self._setupReverseMaps()
