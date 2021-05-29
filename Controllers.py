a5b50aa0cba134af57b6c0772fe816dd4453490d
Spoonieau <spoonie.woonie@gmail.com>
2021-05-29 11:39:11 +0800

    Added more controller support
    -Steam controller
    -XboxONE
    -GameHat https://www.waveshare.com/wiki/Game_HAT
    -Ipega PG-9099 Wolverine Bluetooth

--- Controllers.py
+++ Controllers.py
@@ -285,6 +285,57 @@ class MMP1251(Gamepad):
         }
         self._setupReverseMaps()
 
+class GameHat(Gamepad):
+    fullName = "WaveShare rpi GameHat "
+
+    def __init__(self, joystickNumber = 0):
+        Gamepad.__init__(self, joystickNumber)
+        self.axisNames = {
+            0: 'LEFT-X',
+            1: 'LEFT-Y'
+        }
+        self.buttonNames = {
+            0:  'A',
+            1:  'B',
+            2:  'X',
+            3:  'Y',
+            4:  'TR',
+            5:  'TL',
+            6:  'SELECT',
+            7:  'START'
+        }
+        self._setupReverseMaps()
+
+class PG9099(Gamepad):
+    fullName = 'ipega PG-9099 Bluetooth Controller'
+
+    def __init__(self, joystickNumber = 0):
+        Gamepad.__init__(self, joystickNumber)
+        self.axisNames = {
+            0: 'LAS -X', #Left Analog Stick Left/Right
+            1: 'LAS -Y', #Left Analog Stick Up/Down
+            2: 'RAS -X', #Right Analog Stick Left/Right
+            3: 'RAS -Y', #Right Analog Stick Up/Down
+            4: 'RT', #Right Trigger
+            5: 'LT', #Left Trigger
+            6: 'DPAD -X', #D-Pad Left/Right
+            7: 'DPAD -Y' #D-Pad Up/Down
+        }
+        self.buttonNames = {
+            0:  'A', #A Button
+            1:  'B', #B Button
+            3:  'X', #X Button
+            4:  'Y', #Y Button
+            6:  'LB', #Left Bumper
+            7:  'RB', #Right Bumper
+            10: 'SELECT', #Select Button
+            11: 'START', #Hamburger Button
+            13: 'LASB', #Left Analog Stick button
+            14: 'RASB' #Right Analog Stick button
+                
+        }
+        self._setupReverseMaps()
+    
 
 class example(Gamepad):
     # This class must have self.axisNames with a map
